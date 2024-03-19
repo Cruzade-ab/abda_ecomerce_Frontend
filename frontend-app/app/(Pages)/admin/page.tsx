f2b71d8be8f26121a48584c9260f1b58a6ccd946
Brian Morales <brianmorales1998s@gmail.com>
2024-03-18 20:53:01 -0400

    ningun cambio

--- frontend-app/app/(Pages)/admin/page.tsx
+++ frontend-app/app/(Pages)/admin/page.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/app/components/home/navBar/Navbar"
 export default function Admin (){
     return(
         <>
+        
         <h1 className="w-full bg-black text-white text-center">Esta pagina es para crear Productos</h1>
         <AdminForm/>
         </>
