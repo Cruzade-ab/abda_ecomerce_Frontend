0cda0e7bdb1438f8259a0098efe14cbb8c42bca0
Abima <abimael.santa1@gmail.com>
2024-03-31 13:41:49 -0400

    .Search page Navbar updated

--- frontend-app/app/(Pages)/search/page.tsx
+++ frontend-app/app/(Pages)/search/page.tsx
@@ -1,13 +1,46 @@
 "use client"
 
 import Navbar from "@/app/components/home/navBar/Navbar"
+import { useState, useEffect } from "react";
+export default function Search() {
 
-export default function Search(){
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    const [apiUrl, setApiUrl] = useState('http://localhost:4000/api/products/getAllProducts');
+    const [sectionName, setSectionName] = useState('Most Wanted Products');
 
 
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('http://localhost:4000/api/user/getUser', {
+                    credentials: "include",
+                });
+                if (response.ok) {
+                    const content = await response.json();
+                    setIsLoggedIn(true);
+                    setIsAdmin(content.role_id === 2);
+                    console.log(content);
+                } else {
+                    setIsLoggedIn(false);
+                    setIsAdmin(false);
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+                setIsLoggedIn(false);
+                setIsAdmin(false);
+            }
+        })();
+    }, []);
 
-    return(
-    <Navbar/>
-    
+    const handleCategoryChange = (category: string) => {
+        setApiUrl(`http://localhost:4000/api/products/${category}`);
+        setSectionName(category === 'men' ? 'Men\'s Collection' : 'Women\'s Collection');
+    };
+    return (
+        <Navbar onCategoryChange={handleCategoryChange} isAdmin={isAdmin} />
+
     )
 }
