9e378f45f97dc3cf15a6f5d91a8e0deeb356a329
Abima <abimael.santa1@gmail.com>
2024-03-31 13:01:00 -0400

    Admin Page added

--- frontend-app/app/(Pages)/admin/page.tsx
+++ frontend-app/app/(Pages)/admin/page.tsx
@@ -2,12 +2,57 @@
 
 import AdminForm from "@/app/components/admin/AdminForm"
 import Navbar from "@/app/components/home/navBar/Navbar"
+import { useState, useEffect } from "react"
 
-export default function Admin (){
-    return(
+export default function Admin() {
+    const [message, setMessage] = useState('');
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    const [apiUrl, setApiUrl] = useState('http://localhost:4000/api/products/getAllProducts');
+    const [sectionName, setSectionName] = useState('Most Wanted Products');
+
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch('http://localhost:4000/api/user/getUser', {
+                    credentials: "include",
+                });
+                if (response.ok) {
+                    const content = await response.json();
+                    setMessage(`${content.name} ${content.last_name} is logged in with an email: ${content.email}. Its roles is the # ${content.role_id}`);
+                    setIsLoggedIn(true);
+                    setIsAdmin(content.role_id === 2);
+                    console.log(content);
+                } else {
+                    setIsLoggedIn(false);
+                    setIsAdmin(false);
+                    setMessage('You need to log in.');
+                }
+            } catch (error) {
+                console.error('Error fetching user data:', error);
+                setIsLoggedIn(false);
+                setIsAdmin(false);
+                setMessage('You need to log in.');
+            }
+        })();
+    }, []);
+
+    const handleCategoryChange = (category: string) => {
+        setApiUrl(`http://localhost:4000/api/products/${category}`);
+        setSectionName(category === 'men' ? 'Men\'s Collection' : 'Women\'s Collection');
+    };
+
+
+    return (
         <>
-        <h1 className="w-full bg-black text-white text-center">Esta pagina es para crear Productos</h1>
-        <AdminForm/>
+            <Navbar onCategoryChange={handleCategoryChange} />
+            <p className="text-center">
+                {isLoggedIn ? message : 'You need to log in.'}
+            </p>
+            <h1 className="w-full bg-black text-white text-center">Admin Page</h1>
+            <AdminForm />
         </>
     )
 }
