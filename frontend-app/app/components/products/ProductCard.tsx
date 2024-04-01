b0088dfe99d25b69f37801d35cc3db7a4a80ae66
Abima <abimael.santa1@gmail.com>
2024-04-01 19:22:17 -0400

    ProductCard

--- frontend-app/app/components/products/ProductCard.tsx
+++ frontend-app/app/components/products/ProductCard.tsx
@@ -91,17 +91,23 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                     </div>
                     <div className="px-5 pb-5">
                         <h5 className="text-xl tracking-tight text-slate-900">
-                            {product.general_product_name}, {product.brand.brand_name}
+                            <span className='font-medium'>
+                            {product.general_product_name},  
+                            </span>
+                            <span className='italic mx-1'>
+                            { product.brand.brand_name}
+                            </span>
                         </h5>
                     </div>
                
             </div>
-            <div className="mt-4 px-5 pb-5">
-                <div className="mt-2 mb-5 flex items-center justify-between">
+            <div className=" px-5 `">
+                <div className="flex items-center justify-between">
                     <span className="text-3xl font-bold text-slate-900">
                         ${selectedVariant?.value}
                     </span>
-                    <div>
+                    <div className='flex items-center justify-between'>
+                        <div>
                         <p>Size:</p>
                         <select value={selectedSize} onChange={e => handleSizeChange(e.target.value)}>
                             {uniqueSizes.map(size => (
@@ -110,6 +116,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                                 </option>
                             ))}
                         </select>
+                        </div>
+                        <div>
                         <p>Color:</p>
                         <select value={selectedColor} onChange={e => handleColorChange(e.target.value)}>
                             {uniqueColors.map(color => (
@@ -118,11 +126,15 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
                                 </option>
                             ))}
                         </select>
+                        </div>
+                        
                     </div>
                 </div>
-                <button className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-                    Add to cart
-                </button>
+                <a href="#" className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</a>
             </div>
         </div>
     );
