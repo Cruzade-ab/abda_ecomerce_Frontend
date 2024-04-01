e1d923f3ce9ea183ce4422e885a910bb3f40100a
Abima <abimael.santa1@gmail.com>
2024-03-31 20:10:01 -0400

    Navbar Routing

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -39,11 +39,21 @@ export default function Home() {
       }
     })();
   }, []);
-
-  const handleCategoryChange = (category:string) => {
-    setApiUrl(`http://localhost:4000/api/products/${category}`);
-    setSectionName(category === 'men' ? 'Men\'s Collection' : 'Women\'s Collection');
-  };
+  const handleCategoryChange = (category: string) => {
+    let sectionName = '';
+    if (category === 'men') {
+        setApiUrl('http://localhost:4000/api/products/men');
+        sectionName = 'Men\'s Collection';
+    } else if (category === 'women') {
+        setApiUrl('http://localhost:4000/api/products/women');
+        sectionName = 'Women\'s Collection';
+    } else {
+        setApiUrl('http://localhost:4000/api/products/wantedProducts');
+        sectionName = 'Most Wanted Products';
+    }
+    setSectionName(sectionName);
+    setApiUrl(`http://localhost:4000/api/products/${category}`)
+  }
 
   return (
     <>
