b8f20e28bbba39cf59e68b027547bd9cb3695a66
Abima <abimael.santa1@gmail.com>
2024-04-12 16:11:15 -0400

    Admin Create Form

--- frontend-app/app/components/admin/CreateProducts/AdminForm.tsx
+++ frontend-app/app/components/admin/CreateProducts/AdminForm.tsx
@@ -80,100 +80,123 @@ const AdminForm = () => {
 
   return (
     <>
-      <h1>Creating New Product: </h1>
-      <form onSubmit={handleSubmit(onSubmit)} className='border-4 rounded-lg border-black p-3 m-3'>
-        <FormField
-          type="text"
-          placeholder="Enter product name"
-          label="Product Name"
-          name="general_product_name"
-          register={register}
-          error={errors.general_product_name}
-          labelStyle={''}
-          inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
-        <FormField
-          type="text"
-          placeholder="Enter brand name"
-          label="Brand Name"
-          name="brand_name"
-          register={register}
-          error={errors.brand_name} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
-        <FormField
-          type="text"
-          placeholder="Enter description"
-          label="Description"
-          name="description"
-          register={register}
-          error={errors.description} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
-        <FormField
-          type="text"
-          placeholder="Enter section"
-          label="Section"
-          name="section"
-          register={register}
-          error={errors.section} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
-
-        {products.map((product, index) => (
-          <div key={index}>
-            <h3>Product {index + 1}</h3>
+      <div className='flex items-center justify-center'>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-2/3 border rounded-lg gap-y-3'>
+          <div className="flex flex-row justify-evenly">
             <FormField
               type="text"
-              placeholder={`Enter product ${index + 1} value`}
-              label={`Product ${index + 1} Value`}
-              name={`products.${index}.value`}
-              register={register} error={undefined} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
+              placeholder="Enter product name"
+              label="Product Name"
+              name="general_product_name"
+              register={register}
+              error={errors.general_product_name}
+              labelStyle={''}
+              inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
             <FormField
               type="text"
-              placeholder={`Enter product ${index + 1} color`}
-              label={`Product ${index + 1} Color`}
-              name={`products.${index}.color_name`}
-              register={register} error={undefined} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
-
-            {['S', 'M', 'L', 'XL'].map((size) => (
-              <FormField
-                type="text"
-                placeholder={`Enter amount for size ${size}`}
-                label={`Size ${size} Amount`}
-                name={`products[${index}].sizes.${size}` as ValidFieldNames}
-                register={register}
-                error={errors.products?.[index]?.sizes?.[size]}
-                labelStyle={''}
-                inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'}
-                inputIcon={''}
-              />
-            ))}
-
-            <Controller
-              name={`products[${index}].imageFile` as keyof MyFormData}
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="file"
-                  onChange={(e) => {
-                    if (e.target.files) field.onChange(e.target.files[0]);
-                  }}
-                />
-              )}
-            />
-            <Controller
-              name={`products[${index}].hoverImageFile` as keyof MyFormData}
-              control={control}
-              render={({ field }) => (
-                <input
-                  type="file"
-                  onChange={(e) => {
-                    if (e.target.files) field.onChange(e.target.files[0]);
-                  }}
-                />
-              )}
-            />
+              placeholder="Enter brand name"
+              label="Brand Name"
+              name="brand_name"
+              register={register}
+              error={errors.brand_name} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
           </div>
-        ))}
 
-        <button type="button" onClick={addProduct}>Add Product</button>
+          <div className="flex flex-row justify-evenly">
+            <FormField
+              type="text"
+              placeholder="Enter description"
+              label="Description"
+              name="description"
+              register={register}
+              error={errors.description} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
+            <FormField
+              type="text"
+              placeholder="Enter section"
+              label="Section"
+              name="section"
+              register={register}
+              error={errors.section} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
+          <div className="flex flex-row justify-center content-center">
+            <div className='flex flex-col w-full'>
+              {products.map((product, index) => (
+                <div key={index} className='flex-row '>
+                  <h3 className='text-center'>Product Details of Variant {index + 1}</h3>
+                  <div className='flex flex-row'>
+                    <div className=" flex flex-col basis-1/2 items-center">
+                      <FormField
+                        type="text"
+                        placeholder={`Enter product ${index + 1} value`}
+                        label={`Product ${index + 1} Value`}
+                        name={`products.${index}.value`}
+                        register={register} error={undefined} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
+                      <FormField
+                        type="text"
+                        placeholder={`Enter product ${index + 1} color`}
+                        label={`Product ${index + 1} Color`}
+                        name={`products.${index}.color_name`}
+                        register={register} error={undefined} labelStyle={''} inputStyle={'w-auto -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'} inputIcon={''} />
+                    </div>
+                    <div className=" flex flex-col basis-1/2 ">
+
+                      <div className='flex flex-row justify-evenly'>
+                        {['S', 'M', 'L', 'XL'].map((size) => (
+                          <FormField
+                            type="text"
+
+                            label={`${size}`}
+                            name={`products[${index}].sizes.${size}` as ValidFieldNames}
+                            register={register}
+                            error={errors.products?.[index]?.sizes?.[size]}
+                            labelStyle={'text-center'}
+                            inputStyle={'w-10 -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500'}
+                            inputIcon={''}
+                          />
+                        ))}
+                      </div>
+
+                      <div className='content-center'>
+                    
+                      <Controller
+                        name={`products[${index}].imageFile` as keyof MyFormData}
+                        control={control}
+                        render={({ field }) => (
+                          <input
+                            type="file"
+                            onChange={(e) => {
+                              if (e.target.files) field.onChange(e.target.files[0]);
+                            }}
+                          />
+                        )}
+                      />
+                      <Controller
+                        name={`products[${index}].hoverImageFile` as keyof MyFormData}
+                        control={control}
+                        render={({ field }) => (
+                          <input
+                            type="file"
+                            onChange={(e) => {
+                              if (e.target.files) field.onChange(e.target.files[0]);
+                            }}
+                          />
+                        )}
+                      />
+                      </div>
+                      
+
+                      
+
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button type="button" onClick={addProduct}>Add Product</button>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </>);
 };
 
