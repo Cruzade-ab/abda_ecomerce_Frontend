a210822ed5c4832b05481aca7b054913ad65e3ed
Abima <abimael.santa1@gmail.com>
2024-05-25 21:01:22 -0400

    Placeholders e iconos del formulario de checkout corregidos

--- frontend-app/app/components/order/orderForm.tsx
+++ frontend-app/app/components/order/orderForm.tsx
@@ -61,55 +61,55 @@ function OrderForm() {
       <div className="bg-gray-100 text-gray-600 rounded-3xl shadow-xl px-4 py-10">
         <h1 className="font-bold text-3xl text-gray-900 mb-6">Processing Order</h1>
         <p className="mb-2">Please enter your information to confirm your order</p>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="">
           <OrderFormField
             label="Address"
             type="address"
-            placeholder="Enter your address"
+            placeholder="Street #001, KM-062"
             name="address"
             register={register}
             error={errors.address}
             labelStyle="text-xs font-semibold px-1"
             inputStyle="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500"
-            inputIcon="mdi mdi-email-outline text-gray-400 text-lg"
+            inputIcon="mdi mdi-map-marker-outline text-gray-400 text-lg"
           />
           <OrderFormField
             label="City"
             type="city"
-            placeholder="Select your city"
+            placeholder="Arecibo"
             name="city"
             register={register}
             error={errors.city}
             labelStyle="text-xs font-semibold px-1"
             inputStyle="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500"
-            inputIcon="mdi mdi-lock-outline text-gray-400 text-lg"
+            inputIcon="mdi mdi-home-city text-gray-400 text-lg"
           />
           <OrderFormField
             label="State"
             type="state"
-            placeholder="Select your state"
+            placeholder="Puerto Rico"
             name="state"
             register={register}
             error={errors.state}
             labelStyle="text-xs font-semibold px-1"
             inputStyle="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500"
-            inputIcon="mdi mdi-lock-outline text-gray-400 text-lg"
+            inputIcon="mdi mdi-city-variant-outline text-gray-400 text-lg"
           />
           <OrderFormField
             label="Zip Code"
             type="zip_code"
-            placeholder="Enter Zip Code"
+            placeholder="00372"
             name="zip_code"
             register={register}
             error={errors.zip_code}
             labelStyle="text-xs font-semibold px-1"
             inputStyle="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-gray-500"
-            inputIcon="mdi mdi-lock-outline text-gray-400 text-lg"
+            inputIcon="mdi mdi-map-marker text-gray-400 text-lg"
           />
-          <div className="flex justify-center">
+          {/* <div className="flex justify-center mt-6">
             <p>Do you want to edit your cart? <Link href="/cart" className="text-blue-500 hover:underline">Cart</Link></p>
-          </div>
-          <button type="submit" className="flex items-center justify-center w-full bg-green-500 hover:bg-green-600 focus:bg-green-700 text-white rounded-lg py-3 font-semibold">
+          </div> */}
+          <button type="submit" className=" mt-6 flex items-center justify-center w-full bg-green-500 hover:bg-green-600 focus:bg-green-700 text-white rounded-lg py-3 font-semibold">
             <img src="https://res.cloudinary.com/dcldzjq9s/image/upload/v1714370326/eccomerce_shirt_images/final_cart_icon_yq2qqq.webp" alt="Cart Icon" className="w-6 h-6 mr-2" />
             CONFIRM PURCHASE
           </button>
