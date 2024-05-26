6dcb9149ca849a438b55f3b629f6c5adbfaedf25
DairanDoo <your_email@example.com>
2024-05-25 21:01:55 -0400

    Mejoras en la interfaz del navbar, cambio de color a negro al estar en admin. Cambio de texto (home y cart) por iconos.

--- frontend-app/app/components/home/navBar/Navbar.tsx
+++ frontend-app/app/components/home/navBar/Navbar.tsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 import Search from "../Search bar/search";
 import LoginModal from "@/app/components/cart/LoginModal";
 import { Kaushan_Script } from "next/font/google";
-import { useRouter } from 'next/router'; // Import the useRouter hook
+import Icon from "@mdi/react";
+import { mdiAccountCircle, mdiCartVariant } from "@mdi/js";
+
 const kaushan = Kaushan_Script({ subsets: ["latin"], weight: ["400"] });
 
 interface NavbarProps {
@@ -13,20 +15,16 @@ interface NavbarProps {
   isAdmin: boolean;
 }
 
-
 const Navbar: React.FC<NavbarProps> = ({ onCategoryChange, isAdmin }) => {
-
-
   const fontStyle = {
     fontFamily: kaushan.className,
-    fontSize: "46px",
-    color: "#5B5C31",
-    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.25)", // Drop shadow effect
-    WebkitTextStroke: "1px black", // Stroke effect,
+    fontSize: "36px",
+    color: isAdmin ? "#FFFFFF" : "#5B5C31", // Invert text color for admin
+    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.25)",
+    WebkitTextStroke: isAdmin ? "0.5px black" : "1px black",
     fontStyle: "italic",
   };
-  
-  
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [navbar, setNavbar] = useState(false);
   const [showLoginModal, setShowLoginModal] = useState(false);
@@ -38,83 +36,69 @@ const Navbar: React.FC<NavbarProps> = ({ onCategoryChange, isAdmin }) => {
 
   const handleCartClick = () => {
     if (!isLoggedIn) {
-      // Show the login modal only if the user is not logged in
-      console.log("User is not logged in, showing login modal");
       setShowLoginModal(true);
     } else {
-      // Redirect to the cart page using Next.js router
-      window.location.href = '/cart'; // Use window.location.href for client-side navigation
+      window.location.href = "/cart";
     }
   };
-  
-  
+
   return (
     <div>
-      <nav className="W-full h-16 bg-white border-b-2 border-gray-300 fixed top-0 left-0 right-0 ">
-        <div className="justify-between px-2 mx-auto lg:max-w-7xl md:items center md:flex md:px-8">
-          <div>
-            <div className="flex items-center justify-between py-2 md:py-5 md:block">
-              {/*Logo*/}
-              <Link href="/" onClick={() => { onCategoryChange('wantedProducts'); setNavbar(false); }} >
-                <h2 className="text-xl pb-8" style={fontStyle}>
-                  ABDA Shirts
-                </h2>
-              </Link>
-              {/*hamburger button for mobile*/}
-              <div className="md:hidden">
-                <button
-                  className="p-2 focus:border-gray-400 "
-                  onClick={() => setNavbar(!navbar)}
-                >
-                  {navbar ? (
-                    <Image src="/icons8-close.svg" width={30} height={30} alt="close icon" />
-                  ) : (
-                    <Image src="/icons8-hamburger.svg" width={30} height={30} alt="logo" className="focus:border-none active:border-none" />
-                  )}
-                </button>
-              </div>
+      <nav className={`w-full h-18 ${isAdmin ? "bg-black text-white" : "bg-white text-black"} border-b-2 ${isAdmin ? "border-white" : "border-gray-300"} fixed top-0 left-0 right-0 z-50`}>
+        <div className="flex justify-between items-center px-4 mx-auto lg:max-w-7xl md:px-8">
+          <div className="flex items-center py-2">
+            <Link href="/" onClick={() => { onCategoryChange('wantedProducts'); setNavbar(false); }}>
+              <h2 className="text-xl" style={fontStyle}>ABDA Shirts</h2>
+            </Link>
+            <div className="md:hidden ml-4">
+              <button
+                className="p-2 focus:outline-none"
+                onClick={() => setNavbar(!navbar)}
+                aria-label="Toggle navigation"
+              >
+                {navbar ? (
+                  <Image src="/icons8-close.svg" width={30} height={30} alt="Close icon" />
+                ) : (
+                  <Image src="/icons8-hamburger.svg" width={30} height={30} alt="Menu icon" />
+                )}
+              </button>
             </div>
           </div>
-          {/* Search bar */}
-          <div className="text-xl">
+          <div className="hidden md:block">
             <Search />
           </div>
-          {/* Navbar links */}
-          <div>
-            <div className={`${navbar ? "block" : "hidden"} md:flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0`}>
-              <ul className="h-screen md:h-auto items-center justify-center md:flex md:items-center md:justify-center">
-                <li className="text-xl text-black py-3 md:px-6 text-center hover:bg-gray-400 md:hover:text-gray-400 md:hover:bg-transparent">
-                  <Link href="/" onClick={() => { onCategoryChange('men'); setNavbar(false); }}>Men</Link>
+          <div className={`md:flex ${navbar ? "block" : "hidden"} w-full md:w-auto mt-4 md:mt-0`}>
+            <ul className="flex flex-col md:flex-row items-center md:space-x-8 mt-4 md:mt-0">
+              {isAdmin && (
+                <li className="text-xl py-2 md:py-0 hover:text-gray-400">
+                  <Link href="/admin" onClick={() => setNavbar(false)}>Admin</Link>
                 </li>
-                <li className="text-xl text-black py-3 md:px-6 text-center hover:bg-gray-400 md:hover:text-gray-400 md:hover:bg-transparent">
-                  <Link href="/" onClick={() => { onCategoryChange('women'); setNavbar(false); }}>Women</Link>
-                </li>
-                <li className="text-xl text-black py-3 md:px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-400 border-gray-400 md:hover:text-gray-400 md:hover:bg-transparent">
-                  <Link href={isLoggedIn ? "/profile" : "/login"} onClick={() => setNavbar(!navbar)}>Account</Link>
-                </li>
-                <li className="text-xl text-black py-3 md:px-6 text-center border-b-2 md:border-b-0 hover:bg-gray-400 border-gray-400 md:hover:text-gray-400 md:hover:bg-transparent">
-                  {/* Handle cart click */}
-                  <button><a onClick={handleCartClick}>Cart</a></button>
-                </li>
-                {/* Admin link */}
-                {isAdmin && (
-                  <li className="text-xl text-black py-3 md:px-6 text-center hover:bg-gray-400 md:hover:text-gray-400 md:hover:bg-transparent">
-                    <Link href="/admin" onClick={() => setNavbar(false)}>Admin</Link>
-                  </li>
-                )}
-              </ul>
-            </div>
+              )}
+              <li className="text-xl py-2 md:py-0 hover:text-gray-400">
+                <Link href="/" onClick={() => { onCategoryChange('men'); setNavbar(false); }}>Men</Link>
+              </li>
+              <li className="text-xl py-2 md:py-0 hover:text-gray-400">
+                <Link href="/" onClick={() => { onCategoryChange('women'); setNavbar(false); }}>Women</Link>
+              </li>
+              <li className="text-xl py-2 md:py-0 hover:text-gray-400">
+                <Link href={isLoggedIn ? "/profile" : "/login"} onClick={() => setNavbar(!navbar)}>
+                  <Icon path={mdiAccountCircle} size={1.3} className="text-current" />
+                </Link>
+              </li>
+              <li className="text-xl py-2 md:py-0 hover:text-gray-400">
+                <button onClick={handleCartClick}>
+                  <Icon path={mdiCartVariant} size={1.3} className="text-current" />
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
-      {/* Render the login modal only if showLoginModal state is true */}
       {showLoginModal && (
         <LoginModal
           isOpen={showLoginModal}
           onClose={() => setShowLoginModal(false)}
-          onLogin={() => {
-            setShowLoginModal(false);
-          }}
+          onLogin={() => setShowLoginModal(false)}
         />
       )}
     </div>
